905e452ade902593bff73e69644be6a71c220d92
DaDrwin <darienjensen@outlook.com>
2024-02-19 11:43:19 -0700

    navbar and routing

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,52 +1,84 @@
 <template>
-  <span class="navbar-text">
-    <button class="btn selectable text-success lighten-30 text-uppercase my-2 my-lg-0" @click="login"
-      v-if="!user.isAuthenticated">
-      Login
-    </button>
-    <div v-else>
-      <div class="dropdown my-2 my-lg-0">
-        <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
-          <div v-if="account.picture || user.picture">
-            <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
-          </div>
-        </div>
-        <div class="dropdown-menu dropdown-menu-sm-end dropdown-menu-start p-0" aria-labelledby="authDropdown">
-          <div class="list-group">
-            <router-link :to="{ name: 'Account' }">
-              <div class="list-group-item dropdown-item list-group-item-action">
-                Manage Account
-              </div>
-            </router-link>
-            <div class="list-group-item dropdown-item list-group-item-action text-danger selectable" @click="logout">
-              <i class="mdi mdi-logout"></i>
-              logout
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </span>
+	<span class="navbar-text">
+		<span
+			class="selectable text-secondary fs-3"
+			@click="login"
+			v-if="!user.isAuthenticated">
+			Login
+		</span>
+		<div v-else>
+			<div class="dropdown-center">
+				<button
+					class="picture rounded-circle dropdown-toggle border-0 p-0"
+					data-bs-toggle="dropdown"
+					aria-expanded="false">
+					<img
+						v-if="account.picture || user.picture"
+						:src="account.picture || user.picture"
+						alt="account photo"
+						height="40"
+						class="img-fluid rounded-circle" />
+				</button>
+				<ul class="dropdown-menu px-1">
+					<li>
+						<router-link :to="{ name: 'Home' }">
+							<div class="dropdown-item selectable text-secondary">
+								<i class="mdi mdi-home"></i>
+								Home
+							</div>
+						</router-link>
+					</li>
+					<li>
+						<router-link :to="{ name: 'Account' }">
+							<div class="dropdown-item selectable text-secondary">
+								<i class="mdi mdi-account-cog"></i> Manage Account
+							</div>
+						</router-link>
+					</li>
+					<li>
+						<router-link :to="{ name: 'Profile', params: { id: 'a' } }">
+							<div class="dropdown-item selectable text-secondary">
+								<i class="mdi mdi-account-search"></i>View Profile
+							</div>
+						</router-link>
+					</li>
+					<li>
+						<div
+							class="dropdown-item selectable text-danger"
+							@click="logout">
+							<i class="mdi mdi-logout"></i>
+							logout
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</span>
 </template>
 
 <script>
-import { computed } from 'vue'
-import { AppState } from '../AppState'
-import { AuthService } from '../services/AuthService'
+import { computed } from "vue";
+import { AppState } from "../AppState";
+import { AuthService } from "../services/AuthService";
 export default {
-  setup() {
-    return {
-      user: computed(() => AppState.user),
-      account: computed(() => AppState.account),
-      async login() {
-        AuthService.loginWithPopup()
-      },
-      async logout() {
-        AuthService.logout({ returnTo: window.location.origin })
-      }
-    }
-  }
-}
+	setup() {
+		return {
+			user: computed(() => AppState.user),
+			account: computed(() => AppState.account),
+			async login() {
+				AuthService.loginWithPopup();
+			},
+			async logout() {
+				AuthService.logout({ returnTo: window.location.origin });
+			},
+		};
+	},
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.picture {
+	aspect-ratio: 1;
+	height: 6vh;
+}
+</style>
---
+++ client/src/pages/ProfilePage.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="component">Profile Page</div>
+</template>
+
+<script>
+import { AppState } from "../AppState";
+import { computed, ref, onMounted } from "vue";
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
