09d76c4268202aec3455f8b232ae4f9d6e2a41b0
DaDrwin <darienjensen@outlook.com>
2024-03-03 06:13:26 -0700

    idk

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,8 @@
 export const dev = window.location.origin.includes("localhost");
-export const baseURL = dev ? "https://localhost:7045" : "https://keeper.moresus.com";
+export const baseURL = dev
+	? "https://localhost:7045"
+	: "https://keeper.moresus.com";
 export const useSockets = false;
-export const domain = ${{secrets.AUTH0_DOMAIN}};
-export const clientId = ${secrets.CLIENTID};
-export const audience = ${{secrets.AUTH0_AUDIENCE}};
+export const domain = secrets.AUTH0_DOMAIN;
+export const clientId = secrets.CLIENTID;
+export const audience = secrets.AUTH0_AUDIENCE;
